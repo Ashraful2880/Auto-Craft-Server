276db8bc59ced5b406031fbe810f6dc8b9244b59
Ashraful <ashrafulislam2880@gmail.com>
2022-02-13 23:33:26 +0600

    SSL Commerz API Added

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const app=express();
 const port=process.env.PORT || 5000;
 const cors=require('cors');
 const fileUpload=require('express-fileupload');
-const SSLCommerzPayment = require('sslcommerz')
+const SSLCommerzPayment = require('sslcommerz');
 
 app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
+app.use(express.urlencoded({ extended: true }));
 
 
 //<------------- Database Code Here ---------->
@@ -184,6 +185,71 @@ app.use(fileUpload());
           console.log(remove);
           res.json(remove)
         });
+
+        //<----------- SSL Commerz API----------->
+
+        app.get('/init/:amount', (req, res) => {
+          const data = {
+              total_amount: req.params.amount,
+              currency: 'BDT',
+              tran_id: 'REF123',
+              success_url: 'http://localhost:5000/success',
+              fail_url: 'http://localhost:5000/fail',
+              cancel_url: 'http://localhost:5000/cancel',
+              ipn_url: 'http://localhost:5000/ipn',
+              shipping_method: 'Courier',
+              product_name: 'Computer.',
+              product_category: 'Electronic',
+              product_profile: 'general',
+              cus_name: 'Customer Name',
+              cus_email: 'cust@yahoo.com',
+              cus_add1: 'Dhaka',
+              cus_add2: 'Dhaka',
+              cus_city: 'Dhaka',
+              cus_state: 'Dhaka',
+              cus_postcode: '1000',
+              cus_country: 'Bangladesh',
+              cus_phone: '01711111111',
+              cus_fax: '01711111111',
+              ship_name: 'Customer Name',
+              ship_add1: 'Dhaka',
+              ship_add2: 'Dhaka',
+              ship_city: 'Dhaka',
+              ship_state: 'Dhaka',
+              ship_postcode: 1000,
+              ship_country: 'Bangladesh',
+              multi_card_name: 'mastercard',
+              value_a: 'ref001_A',
+              value_b: 'ref002_B',
+              value_c: 'ref003_C',
+              value_d: 'ref004_D'
+          };
+          const sslcommer = new SSLCommerzPayment(process.env.STORE_ID,process.env.STORE_PASSWORD,false)
+            sslcommer.init(data).then(data => {
+            res.redirect(data.GatewayPageURL)
+          });
+      })
+
+          //<-------- SSLCommerz Success API------------>
+
+      app.post('/success', (req,res)=>{
+        console.log(req.body);
+        res.status(200).json(req.body);
+      })
+      
+         //<-------- SSLCommerz Fail API------------>
+
+      app.post('/fail', (req,res)=>{
+        console.log(req.body);
+        res.status(400).json(req.body);
+      })
+
+        //<-------- SSLCommerz Cancel API------------>
+
+      app.post('/cancel', (req,res)=>{
+        console.log(req.body);
+        res.status(200).json(req.body);
+      })
         
       } finally {
         // await client.close();
